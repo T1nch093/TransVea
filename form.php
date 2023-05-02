e6a48e61a11e18d8ba956599af5725eae8a2d3d2
T1nch093 <130597371+T1nch093@users.noreply.github.com>
2023-05-02 18:44:10 -0300

    probando php

---
+++ form.php
@@ -0,0 +1,35 @@
+<?php
+$nombre = $_POST["nombre"];
+$email = $_POST["email"]:
+$telefono = $_POST["telefono"];
+$ubicacion = $_POST["ubicacion"];
+$recorrido = $_POST["recorrido"]:
+$mensaje = $_POST["descripcion"];
+
+//Como llega el cuerpo del mail.
+$mensaje = "Nombre" . $nombre . "\r\n";
+$mensaje .= "Email" . $email . "\r\n";
+$mensaje .= "Telefono" . $telefono . "\r\n"
+$mensaje .= "Ubicacion" . $ubicacion . "\r\n"
+$mensaje .= "Recorrido" . $recorrido . "\r\n"
+$mensaje .= "Descripcion" . $mensaje . "\r\n";
+$mensaje .= "Enviado el " . date["d/m/y", time()];
+
+$enlace = mysql_connect($nombre, $email, $telefono, $ubicacion, $recorrido, $descripcion)
+$insertarDatos = "INSERT INTO datos VALUES ("$nombre" "$email" "$telefono" "$ubicacion" "$recorrido" "$descripcion")"
+$ejecutarInsertion = mysqli_query($enlace, $insertarDatos);
+if(!$ejecutarInsertion){
+    echo "error";
+}
+$para = "martinservetti@gmail.com"; //mail donde llega el fomulario
+$asunto = "Pedido de cotizacion de". $nombre; //Asunto del mail
+
+//la funcion mail envia un correo electronico y el orden es:
+// A quien se lo envia - El titulo del correo - El mensaje - Header para añadir.
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+// Redireccion al haber enviado el form
+header("Location:index.html");
+
+
+?>
